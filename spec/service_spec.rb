5f331cd3f7eccc5b25e73cd5607447787d048e14
haazime <h4zime@gmail.com>
2017-12-14 19:39:52 +0900

    fix

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 require 'article_service'
 
-RSpec.describe 'call application service' do
-  it do
-    user = double(:user)
-    params = double(:params)
+RSpec.describe Oblate::Service do
+  describe 'call as class method' do
+    it do
+      user = double(:user)
+      params = double(:params)
 
-    expect_any_instance_of(ArticleService).to receive(:post).with(user, params)
-    ArticleService.post(user, params)
+      expect_any_instance_of(ArticleService).to receive(:post).with(user, params)
+      ArticleService.post(user, params)
+    end
   end
 end
