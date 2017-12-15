a8450ad4a8867b41e5abc2b4703719bf2bdbd762
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-12-15 14:20:17 +0900

    failed with exception

--- lib/oblate/service.rb
+++ lib/oblate/service.rb
@@ -12,9 +12,40 @@ module Oblate
         end
 
         def method_missing(method, *args, &block)
-          return super unless respond_to_missing?(method)
+          method_in_str = method.to_s
+
+          with_bang = false
+          candidate =
+            if method_in_str.end_with?('!')
+              with_bang = true
+              method_in_str.chop.to_sym
+            else
+              method
+            end
+
+          return super unless respond_to_missing?(candidate)
+          return call_service_method!(candidate, *args, &block) if with_bang
+          call_service_method(candidate, *args, &block)
+        end
+
+        def call_service_method!(method, *args, &block)
+          result = call_service_method(method, *args, &block)
+          raise ExecutionFailed.new(result) if result.failed?
+          result
+        end
+
+        def call_service_method(method, *args, &block)
           new.send(method, *args, &block)
         end
+    end
+  end
+
+  class ExecutionFailed < StandardError
+    attr_reader :result
+
+    def initialize(result)
+      @result = result
+      super('Failed to execution')
     end
   end
 end
--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -33,12 +33,22 @@ RSpec.describe Oblate::Service do
     it do
       allow(params).to receive(:valid?) { false }
 
-      service = service_class.new(model: Article)
-      result = service.post(user, params)
+      result = service_class.post(user, params)
 
       aggregate_failures do
         expect(result).to_not be_succeeded
         expect(result.params).to eq(params)
+      end
+    end
+  end
+
+  describe 'failed by exception' do
+    it do
+      allow(params).to receive(:valid?) { false }
+
+      expect { service_class.post!(user, params) }.to raise_error do |e|
+        expect(e).to be_a(Oblate::ExecutionFailed)
+        expect(e.result.params).to eq(params)
       end
     end
   end
